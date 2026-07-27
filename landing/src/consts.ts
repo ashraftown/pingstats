3f8c95e7ce7ac3f7944eff22c7bb781868544bad
Ashraf Town <53049426+ashraftown@users.noreply.github.com>
2026-07-27 13:12:58 +0200

    landing review fixes: constants module, btn heights, motion pref, social meta, various tweaks

---
+++ landing/src/consts.ts
@@ -0,0 +1,3 @@
+export const GITHUB_REPO = "https://github.com/ashraftown/pingstats";
+export const MACOS_DMG = `${GITHUB_REPO}/releases/latest/download/PingStats.dmg`;
+export const WINDOWS_ZIP = `${GITHUB_REPO}/releases/latest/download/PingStats-windows.zip`;
